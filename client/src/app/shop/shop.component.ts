5e425b012d0e7099e4bfe671e0e76a06151f34b8
trombone8417 <trombone8417@yahoo.com>
2021-08-06 06:19:05 +0800

    Pager

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IBrand } from '../models/brand';
 import { IProduct } from '../models/product';
 import { IType } from '../models/productType';
+import { ShopParams } from '../models/ShopParams';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -10,10 +11,17 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-
-  products:IProduct[]=[];
-  brands:IBrand[]=[];
-  types:IType[]=[];
+  @ViewChild('search', { static: true}) searchTerm:ElementRef;
+  products:IProduct[];
+  brands:IBrand[];
+  types:IType[];
+  shopParams=new ShopParams();
+  totalCount:number;
+  sortOptions = [
+    {name:'Alphabetical', value:'name'},
+    {name:'Price: Low to High', value:'priceAsc'},
+    {name:'Price: High to Low', value:'priceDesc'}
+  ];
 
   constructor(private shopService: ShopService) { }
 
@@ -24,8 +32,11 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts(){
-    this.shopService.getProducts().subscribe(response=>{
+    this.shopService.getProducts(this.shopParams).subscribe(response=>{
       this.products = response.data;
+      this.shopParams.pageNumber = response.pageIndex;
+      this.shopParams.pageSize = response.pageSize;
+      this.totalCount = response.count;
     },error=>{
       console.log(error);
     }); 
@@ -33,7 +44,7 @@ export class ShopComponent implements OnInit {
 
   getBrands(){
     this.shopService.getBrands().subscribe(response=>{
-      this.brands = response;
+      this.brands = [{id:0,name:'All'},...response];
     },error=>{
       console.log(error);
     });
@@ -41,9 +52,45 @@ export class ShopComponent implements OnInit {
 
   getTypes(){
     this.shopService.getTypes().subscribe(response=>{
-      this.types = response;
+      this.types = [{id:0,name:'All'},...response];
     },error=>{
       console.log(error);
     });
+  }
+  
+  onBrandSelected(brandId:number){
+    this.shopParams.brandId=brandId;
+    this.shopParams.pageNumber=1;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId:number){
+    this.shopParams.typeId=typeId;
+    this.shopParams.pageNumber=1;
+    this.getProducts();
+  }
+
+  onSortSelected(sort:string){
+    this.shopParams.sort=sort;
+    this.getProducts();
+  }
+
+  onPageChanged(event:any){
+    if (this.shopParams.pageNumber!==event) {
+      this.shopParams.pageNumber=event;
+      this.getProducts();
+    }
+  }
+
+  onSearch(){
+    this.shopParams.search=this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber=1;
+    this.getProducts();
+  }
+
+  onReset(){
+    this.searchTerm.nativeElement.value='';
+    this.shopParams=new ShopParams();
+    this.getProducts();
   }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,27 +1,54 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IBrand } from '../models/brand';
 import { IPagination } from '../models/pagination';
 import { IType } from '../models/productType';
+import { map } from 'rxjs/operators';
+import { ShopParams } from '../models/ShopParams';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  baseUrl='https://localhost:5001/api/';
+  baseUrl = 'https://localhost:5001/api/';
 
   constructor(private http: HttpClient) { }
 
-  getProducts(){
-    return this.http.get<IPagination>(this.baseUrl+'products?pageSize=50');
+  getProducts(shopParams:ShopParams) {
+    let params = new HttpParams();
+
+    if (shopParams.brandId!==0) {
+      params = params.append('brandId', shopParams.brandId.toString());
+    }
+
+    if (shopParams.typeId!==0) {
+      params = params.append('typeId', shopParams.typeId.toString());
+    }
+
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
+    }
+
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex',shopParams.pageNumber.toString());
+    params = params.append('pageIndex',shopParams.pageSize.toString());
+    
+
+    return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
   }
 
-  getBrands(){
-    return this.http.get<IBrand[]>(this.baseUrl+'products/brands');
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
   }
 
-  getTypes(){
-    return this.http.get<IType[]>(this.baseUrl+'products/types');
+  getTypes() {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
+
 }
