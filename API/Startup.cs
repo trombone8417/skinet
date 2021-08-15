f1a9790ed2b4fc239818d2d6896542338ed890ec
trombone8417 <trombone8417@yahoo.com>
2021-08-15 08:37:39 +0800

    AccountController

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,7 @@ namespace API
             });
 
             services.AddApplicationServices();
+            services.AddIdentityServices();;
             services.AddSwaggerDocumentation();
             services.AddCors(opt=>
             {
