46c04c6f0ce64540e3f633900a6742a61c06288f
trombone8417 <trombone8417@yahoo.com>
2021-09-09 06:23:56 +0800

    Dealing with modelstate errors

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -10,6 +10,7 @@ import { AccountService } from '../account.service';
 })
 export class RegisterComponent implements OnInit {
   registerForm:FormGroup;
+  errors:string[];
 
   constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) { }
 
@@ -30,6 +31,7 @@ export class RegisterComponent implements OnInit {
       this.router.navigateByUrl('/shop');
     }, error => {
       console.log(error);
+      this.errors = error.errors;
     });
   }
 
