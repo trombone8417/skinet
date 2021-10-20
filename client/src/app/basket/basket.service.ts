b7934334fdc7eb4f511efa53da41ea1743dee5f1
trombone8417 <trombone8417@yahoo.com>
2021-10-21 07:07:53 +0800

    Persisting the shipping price

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -35,6 +35,7 @@ export class BasketService {
     this.shipping = deliveryMethod.price;
     const basket = this.getCurrentBasketValue();
     basket.deliveryMethodId = deliveryMethod.id;
+    basket.shippingPrice = deliveryMethod.price;
     this.calculateTotals();
     this.setBasket(basket);
   }
@@ -44,6 +45,7 @@ export class BasketService {
       .pipe(
         map((basket: IBasket) => {
           this.basketSource.next(basket);
+          this.shipping = basket.shippingPrice;
           this.calculateTotals();
         })
       );
