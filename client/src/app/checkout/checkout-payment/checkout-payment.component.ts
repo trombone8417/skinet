7efd8b79446e9d495880e034e4ee974bca436696
trombone8417 <trombone8417@yahoo.com>
2021-10-23 01:09:03 +0800

    Adding stripe elements to the components

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -7,13 +7,23 @@ import { IBasket } from 'src/app/models/basket';
 import { IOrder } from 'src/app/models/order';
 import { CheckoutService } from '../checkout.service';
 
+declare var Stripe;
+
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
   styleUrls: ['./checkout-payment.component.scss']
 })
-export class CheckoutPaymentComponent implements OnInit {
+export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
   @Input() checkoutForm: FormGroup;
+  @ViewChild('cardNumber',{ static: true}) cardNumberElement: ElementRef;
+  @ViewChild('cardExpiry',{ static: true}) cardExpiryElement: ElementRef;
+  @ViewChild('cardCvc',{ static: true}) cardCvcElement: ElementRef;
+  stripe:any;
+  cardNumber:any;
+  cardExpiry: any;
+  cardCvc: any;
+  cardErrors: any;
 
   constructor(
     private basketService:BasketService, 
@@ -21,7 +31,24 @@ export class CheckoutPaymentComponent implements OnInit {
     private toastr:ToastrService,
     private router:Router) { }
 
-  ngOnInit(){
+  ngAfterViewInit() {
+    this.stripe = Stripe('pk_test_51JkSzKFH6IHDh4o0WNUcMVf5Nei3qCFCg2BvgwpjxzA9d69ZuZ2F4JCE5DVScgH87opBRtWIkfbDNLWqA4KAIjxz00rU5vGqKd');
+    const elements = this.stripe.elements();
+
+    this.cardNumber = elements.create('cardNumber');
+    this.cardNumber.mount(this.cardNumberElement.nativeElement);
+    
+    this.cardExpiry = elements.create('cardExpiry');
+    this.cardExpiry.mount(this.cardExpiryElement.nativeElement);
+    
+    this.cardCvc = elements.create('cardCvc');
+    this.cardCvc.mount(this.cardCvcElement.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.cardNumber.destroy();
+    this.cardExpiry.destroy();
+    this.cardCvc.destroy();
   }
 
   submitOrder(){
